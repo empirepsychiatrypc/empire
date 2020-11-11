b1f7263ebc02965ef8208918d122a13023186dc4
Your Name <bobbynicholson78704@gmail.com>
2020-11-11 12:43:41 -0600

    nav and pills

--- main.js
+++ main.js
@@ -66,3 +66,22 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky")
+    document.querySelector('#logo img').src = '/images/WhiteTransparent.png'
+  } else {
+    navbar.classList.remove("sticky");
+    document.querySelector('#logo img').src = '/images/BlueTransparent.png'
+  }
+}
